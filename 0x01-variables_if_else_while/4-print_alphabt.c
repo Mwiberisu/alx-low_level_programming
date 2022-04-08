0f7d6b9a69dc223bb6f0bb4b83b0c027078aaea0
Mwiberisue <susan.mwiberi@gmail.com>
2022-04-08 13:42:06 -0700

    Prints alphabet while skipping some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+/**
+ * main - entrypoint
+ *Description: prints the alphabet
+ *Return: 0 after printing
+ */
+int main(void)
+{
+	char lowerCase[] = "abcdefghijklmnopqrstuvwxyz";
+	unsigned int a;
+    	for(a = 0; a < strlen(lowerCase); a++)
+    	{
+		if((lowerCase[a] != 'q') && lowerCase[a]!= 'e')
+		{
+			 putchar(lowerCase[a]);
+		}
+     	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@ Contains loop functions:
 1. 0-positive_or_negative.c : Checks if number is positive, negative or zero
 2. 1-last_digit.c: print the last digit of the number stored in the variable
 3. 3-print_alphabets.c: prints the aphabet using putchar
+4. 4-print_alphabt.c:  prints the alphabet in lowercase, followed by a new line.
