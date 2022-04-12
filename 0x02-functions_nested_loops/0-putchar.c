95897a23400387de6fd3d0a2b4408f5abe205999
Mwiberisue <susan.mwiberi@gmail.com>
2022-04-12 16:32:43 -0700

    Added putchar display

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - entrypoint
+ *Description: Checks if n is postive, negative or zero
+ *Return: 0 after printing
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains the below files: 
